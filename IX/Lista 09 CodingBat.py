63ba7dd4b05fc7b8ae22dcfadd7484914c3ad1bf
Eber Junior <eberssjunior12@gmail.com>
2023-12-06 16:05:53 -0300

    Lista 10

--- IX/Lista 09 CodingBat.py
+++ IX/Lista 09 CodingBat.py
@@ -53,12 +53,7 @@ def maior_ponta(nums):
 # retorna a soma dos dois primeiros elementos
 # se a lista tiver menos de dois elementos, soma o que for possível
 def sum2(nums):
-  if len(nums) == 0:
-    return 0
-  elif len(nums) == 1:
-    return nums[0]
-  else:
-    return nums[0] + nums[1]
+  return sum(nums[:2])
 
 
 
